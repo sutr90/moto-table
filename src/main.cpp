bae03d4c7f5f8d6cbc02a00fdbf10e8cdc5c0b1f
Vojtěch Frič <you@example.com>
2021-10-07 22:59:10 +0200

    fixed disable/enable mode, added min endstop

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <OneButton.h>
 
 bool run = false;
+bool down = false;
 
 OneButton btnUp = OneButton(10);
 OneButton btnDown = OneButton(11);
@@ -13,34 +14,26 @@ const int STEP_PIN = 9;
 const int LDIR_PIN = 3;
 const int RDIR_PIN = 2;
 
-void clockwise()
+const int ENDSTOP_MIN = 12;
+
+void dirUp()
 {
   digitalWrite(LDIR_PIN, LOW);
   digitalWrite(RDIR_PIN, HIGH);
 }
 
-void counterclock()
+void dirDown()
 {
   digitalWrite(LDIR_PIN, HIGH);
   digitalWrite(RDIR_PIN, LOW);
 }
 
-void enable()
-{
-  digitalWrite(ENABLE_PIN, HIGH);
-}
-
-void disable()
-{
-  digitalWrite(ENABLE_PIN, LOW);
-}
-
-void turnMotor()
+void turnMotor(int delay = STEP_DELAY_US)
 {
   digitalWrite(STEP_PIN, HIGH);
-  delayMicroseconds(STEP_DELAY_US);
+  delayMicroseconds(delay);
   digitalWrite(STEP_PIN, LOW);
-  delayMicroseconds(STEP_DELAY_US);
+  delayMicroseconds(delay);
 }
 
 void setup()
@@ -49,29 +42,54 @@ void setup()
   pinMode(STEP_PIN, OUTPUT);
   pinMode(LDIR_PIN, OUTPUT);
   pinMode(RDIR_PIN, OUTPUT);
+  pinMode(ENDSTOP_MIN, INPUT_PULLUP);
+
+  pinMode(13, OUTPUT);
 
   btnUp.attachDoubleClick([]()
-                             {
-                               clockwise();
-                               run = true;
-                             });
+                          {
+                            dirUp();
+                            run = true;
+                            down = false;
+                          });
 
   btnDown.attachDoubleClick([]()
-                               {
-                                 counterclock();
-                                 run = true;
-                               });
+                            {
+                              dirDown();
+                              run = true;
+                              down = true;
+                            });
 
   btnUp.attachClick([]()
-                            { run = false; });
+                    { run = false; });
 
   btnDown.attachClick([]()
-                              { run = false; });
+                      { run = false; });
 }
+
+void moveBack()
+{
+  delay(50);
+  dirUp();
+  digitalWrite(13, HIGH);
+  for (int j = 0; j < 5000; j++)
+  {
+    turnMotor(STEP_DELAY_US * 2);
+  }
+
+  digitalWrite(13, LOW);
+}
+
 void loop()
 {
   btnUp.tick();
   btnDown.tick();
+
+  if (digitalRead(ENDSTOP_MIN) == LOW)
+  {
+    run = false;
+    moveBack();
+  }
 
   if (run)
   {
